7ce39bafd2ace2bfe32689ab40dc06b3deb3c583
Alex Savelyev <lesh77906@gmail.com>
2019-09-03 23:20:55 +0300

    Модальное окно (Загрузка файлов), исправлены стили, и перемещены все в один файл для удобства, и так... по мелочам...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
-menu.onclock = function myFuntion() {
-	var x = document.getElementById("myTopnav");
+//menu.onclock = function myFuntion() {
+//	var x = document.getElementById("myTopnav");
+//
+//	if(x.className === "topnav") {
+//		x.className += "responsive";
+//	} else {
+//		x.className = "topnav";
+//	}
+//}
 
-	if(x.className === "topnav") {
-		x.className += "responsive";
-	} else {
-		x.className = "topnav";
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+	modal.style.display = "block";
+	document.body.style.overflow = 'hidden';
+}
+span.onclick = function () {
+	modal.style.display = "none";	
+}
+
+window.onclick = function () {
+	if (event.target == modal) {
+		modal.style.display = "none";
 	}
 }
