9f54090e8a7c7534be7e35b88727331896e91096
Alexey <lesh77906@gmail.com>
2020-01-13 18:00:37 +0300

    now its ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,27 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
+var closeButton = document.getElementById("closeButton");
 
 btn.onclick = function () {
 	modal.style.display = "block";
 	document.body.style.overflow = 'hidden';
 }
+
+closeButton.onclick = function () {
+	modal.style.display = "none";
+	document.body.style.overflow = 'auto';
+	location.replace('index.html');
+}
+/*
+
 span.onclick = function () {
 	modal.style.display = "none";
-	document.body.style.overflow = 'auto';	
+	document.body.style.overflow = 'auto';
+	location.replace('index.html');
+
 }
+*/
 
 window.onclick = function () {
 	if (event.target == modal) {
